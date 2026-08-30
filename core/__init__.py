594d5bef714144bd135e7b775e234fb00f1880bb
JARVIS-NEO-CODER <project.jarvis.neo@gmail.com>
2026-08-30 17:12:02 +0200

    Export planner and agent core services

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,14 @@
 """Core services for J.A.R.V.I.S. NEO."""
 
 from .memory import NeoMemory
+from .action_engine import ActionDefinition, ActionEngine, ActionResult, ControlMode
+from .goal_planner import GoalPlan, GoalPlanner, GoalStep, GoalRunResult
+from .agent_engine import AgentEngine, AgentRunResult
+from .ollama_planner import OllamaPlanner
 
-__all__ = ["NeoMemory"]
+__all__ = [
+    "NeoMemory",
+    "ActionDefinition", "ActionEngine", "ActionResult", "ControlMode",
+    "GoalPlan", "GoalPlanner", "GoalStep", "GoalRunResult",
+    "AgentEngine", "AgentRunResult", "OllamaPlanner",
+]
